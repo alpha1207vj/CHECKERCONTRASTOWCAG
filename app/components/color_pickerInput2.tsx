1a1d6b06972f7f3262d71a234457099cf4669393
alpha1207vj <gide749@gmail.com>
2026-03-11 19:37:11 +0100

    SOLVING INPUT2 BUTTON COPY ANIMATION

--- app/components/color_pickerInput2.tsx
+++ app/components/color_pickerInput2.tsx
@@ -51,10 +51,10 @@ export default function ColorInput1({ color1, onColorChange1 }: any) {
             className={`${roboto.className} w-full h-full text-[15px] p-2 border-2 outline-none rounded-br-md rounded-bl-md caret-gray-brand border-transparent`}
           />
            <div className="flex items-center justify-center pr-2 bg-white rounded-br-md">
-            <button className="cursor-pointer transition-transform duration-150 active:scale-90 flex items-center justify-center w-[35px] h-[35px]  rounded-full lg:hover:bg-[#E8E0DE]">
+            <button  onClick={handleCopy} className="cursor-pointer transition-transform duration-150 active:scale-90 flex items-center justify-center w-[35px] h-[35px]  rounded-full lg:hover:bg-[#E8E0DE]">
               <LuCopy
              
-                onClick={handleCopy}
+                
               />
             </button>
           </div>
